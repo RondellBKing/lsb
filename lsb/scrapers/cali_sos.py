5b3a9ce1948c2033b1cf75bc1d704a8541b32571
Ron King <RondellBKing@gmail.com>
2023-04-17 22:21:56 -0400

    Minor Cali sos updates

--- lsb/scrapers/cali_sos.py
+++ lsb/scrapers/cali_sos.py
@@ -1,11 +1,21 @@
 import logging
-
+from bs4 import BeautifulSoup
 from core.scraper import Scraper
 
 class CALI_SOS(Scraper):
     def __init__(self, start_date=None, delta=5):
         super().__init__(start_date, delta)
         self.county_name = "CALI_SOS"
+    
+    def fetch_table_html(self):
+        '''
+        This method is used to extract the table from the results since this is what we are after.
+        '''
+
+        html = BeautifulSoup(self.browser.page_source, 'html.parser')
+        tbl_html = html.find('table', {'class': 'div-table center-container'})
+
+        return tbl_html        
 
     def parse_table(self, tbl_html):
         lead_list = []
